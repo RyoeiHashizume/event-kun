2d57e2a0cff7a555a5fd8beb69372d565ca8df12
ryoeihashizume <ryoei.hashizume@avasys.jp>
2016-06-22 08:34:23 +0000

    aiueo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+   def configure_permitted_parameters
+    #strong parametersを設定し、usernameを許可
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:user_name, :company_number, :email, :password, :password_confirmation) }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :user_name, presence: true, uniqueness: true
 end
