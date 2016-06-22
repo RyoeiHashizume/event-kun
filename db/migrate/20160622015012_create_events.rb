b4fbd5ebb59fe99bfaa940350f8fa673e78011f5
ryoeihashizume <ryoei.hashizume@avasys.jp>
2016-06-22 05:22:55 +0000

    show

--- db/migrate/20160622015012_create_events.rb
+++ db/migrate/20160622015012_create_events.rb
@@ -1,15 +1,15 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-      t.string :event_name, null: false
-      t.date   :event_date, null: false
-      t.time   :event_time, null: false
-      t.string :event_place, null: false
+      t.string :event_name
+      t.date   :event_date
+      t.time   :event_time
+      t.string :event_place
       t.string :event_contents
-      t.integer :event_money, null: false
-      t.integer :event_numofpeople, null: false
-      t.date   :event_deadline, null: false
-      t.integer :event_kanji_id, null: false
+      t.integer :event_money
+      t.integer :event_numofpeople
+      t.date   :event_deadline
+      t.integer :event_kanji_id
 
       t.timestamps null: false
     end
