41ffc81fd985200a7c10d462b7147989f051bde5
AkihiroMaesaka <akihiro.maesaka@avasys.jp>
2016-06-22 06:08:05 +0000

    commit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,6 +69,8 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:event_name)
+      params.require(:event).permit(:event_name, :event_date, :event_time, 
+                                    :event_place, :event_contents, :event_money, 
+                                    :event_numofpeople, :event_deadline, :event_kanji_id)
     end
 end
