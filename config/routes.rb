8ef507a8ff3b44b839e85c8a36c0b3fbfaea0b6f
ShioriYanagisawa <shiori.yanagisawa@avasys.jp>
2016-06-22 02:54:57 +0000

    sever

--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  
-  root to: ('/events')
+
+  root to: redirect('/events')
 end
