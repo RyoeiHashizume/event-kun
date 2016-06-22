bf64025c2fcb0a2a1441ec457ec26f8070ce3d2b
ryoeihashizume <ryoei.hashizume@avasys.jp>
2016-06-22 13:15:48 +0000

    users changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
 
    def configure_permitted_parameters
     #strong parametersを設定し、usernameを許可
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:user_name, :company_number, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:user_name, :company_number, :email, :kanji, :gender,:password, :password_confirmation) }
   end
 end
