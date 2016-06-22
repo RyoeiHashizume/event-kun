063405f78334a3887e232361f40a697d6545eb96
ShioriYanagisawa <shiori.yanagisawa@avasys.jp>
2016-06-22 23:45:54 +0000

    aa

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160622062126) do
-=======
 ActiveRecord::Schema.define(version: 20160622140113) do
->>>>>>> 237223fbb3daa9e5a5c1d0a010418d972f09937f
 
   create_table "events", force: :cascade do |t|
     t.string   "event_name",        null: false
@@ -38,22 +34,7 @@ ActiveRecord::Schema.define(version: 20160622140113) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+# Could not dump table "users" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
 end
