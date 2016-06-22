e1f11023a536dd85b3f1a493bd70cc928fdf6c7b
AkihiroMaesaka <akihiro.maesaka@avasys.jp>
2016-06-22 01:58:59 +0000

    commit

---
+++ db/migrate/20160622015012_create_events.rb
@@ -0,0 +1,16 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :event_name, null: false
+      t.date   :event_time, null: false
+      t.string :event_place, null: false
+      t.string :event_contents
+      t.integer :event_money, null: false
+      t.integer :event_numofpeople, null: false
+      t.date   :event_deadline, null: false
+      t.integer :event_kanji_id, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
